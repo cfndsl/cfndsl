d0c020293f5dccc71ec4d9e937a08f1083c84bd1
Chris Howe <chris@howeville.com>
2012-12-30 16:53:19 -0600

    better plurals support

---
+++ lib/cfndsl/Plurals.rb
@@ -0,0 +1,28 @@
+module CfnDsl
+  module Plurals
+    ##
+    # Plural names for lists of content objects
+    #
+    
+    @@plurals = { 
+      "Metadata" => "Metadata", 
+      "Property" => "Properties",
+      "Policy" => "Policies"
+    }
+
+    @@singles = {}
+    @@plurals.each_pair { |key,val| @@singles[val] = key }
+
+    def self.pluralize(name)
+      name = name.to_s
+      return @@plurals[name] if( @@plurals.has_key? name )
+      return "#{name}s"
+    end
+
+    def self.singularize(name)
+      name = name.to_s
+      return @@singles[name] if( @@singles.has_key? name )
+      return name[0..-2]
+    end
+  end
+end

fa485dd23b46539f7bfc5c1cdb9536d28b3d883a
Chris Howe <chris.howe@civitaslearning.com>
2013-04-23 12:12:32 -0500

    Added some internal implementation to the Metadata class so that it actually works for something.

--- lib/cfndsl/Metadata.rb
+++ lib/cfndsl/Metadata.rb
@@ -5,6 +5,18 @@ module CfnDsl
   class MetadataDefinition < JSONable
     ## 
     # Handles Metadata objects
+    def initialize(value) 
+      @value = value;
+    end
+
+    def value
+      return @value
+    end
+    
+    def to_json(*a) 
+      @value.to_json(*a)
+    end
+
   end
 
 end

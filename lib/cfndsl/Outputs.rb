527fc238c8468bc432a2e8c8e44751d04158bac8
Dan Rasband <danrasband@gmail.com>
2014-03-20 16:11:08 -0600

    Add Condition to Output

--- lib/cfndsl/Outputs.rb
+++ lib/cfndsl/Outputs.rb
@@ -1,11 +1,11 @@
 require 'cfndsl/JSONable'
 
-module CfnDsl  
+module CfnDsl
   class OutputDefinition < JSONable
     ##
     # Handles Output objects
-    dsl_attr_setter :Value, :Description
-    
+    dsl_attr_setter :Value, :Description, :Condition
+
     def initialize( value=nil)
       @Value = value if value
     end

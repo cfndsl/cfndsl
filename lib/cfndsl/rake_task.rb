96bb7712979c51c7d932e76c68f0c2313d0d5022
Aaron Walker <a.walker@base2services.com>
2015-06-30 13:31:46 +1000

    added rake task with ability to specify multiple templates to render

--- lib/cfndsl/rake_task.rb
+++ lib/cfndsl/rake_task.rb
@@ -1,17 +1,30 @@
 require 'rake'
 require 'rake/tasklib'
-require 'cfndsl/cli'
+require 'cfndsl'
 
 module CfnDsl
   class RakeTask < Rake::TaskLib
-    attr_accessor :opts
+    attr_accessor :cfndsl_opts
+
 
     def initialize(name = nil)
       yield self if block_given?
 
       desc 'Generate Cloudformation' unless ::Rake.application.last_comment
       task(name || :generate) do |t, args|
-        Cli.new(opts || []).execute!
+        cfndsl_opts[:files].each do |opts|
+          extra = cfndsl_opts[:extras] || []
+          verbose = cfndsl_opts[:verbose] && STDERR
+          model = CfnDsl::eval_file_with_extras(opts[:filename], extra, verbose)
+          if opts[:output].nil?
+            verbose.puts("Writing to STDOUT") if verbose
+            STDOUT.puts model.to_json
+          else
+            verbose.puts("Writing to #{opts[:output]}") if verbose
+            output = File.open( File.expand_path(opts[:output]), "w")
+            output.puts model.to_json
+          end
+        end
       end
     end
   end

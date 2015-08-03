56a6e309d317e0504be78fd8cad5c28bae73f353
Aaron Walker <a.walker@base2services.com>
2015-08-03 15:23:53 +1000

    refactored version of rake task

--- lib/cfndsl/rake_task.rb
+++ lib/cfndsl/rake_task.rb
@@ -1,31 +1,53 @@
-require 'rake'
-require 'rake/tasklib'
-require 'cfndsl'
+require "rake"
+require "rake/tasklib"
+require "cfndsl"
 
 module CfnDsl
   class RakeTask < Rake::TaskLib
     attr_accessor :cfndsl_opts
 
-
     def initialize(name = nil)
       yield self if block_given?
 
-      desc 'Generate Cloudformation' unless ::Rake.application.last_comment
-      task(name || :generate) do |t, args|
+      desc "Generate Cloudformation" unless ::Rake.application.last_comment
+      task(name || :generate) do |_t, _args|
         cfndsl_opts[:files].each do |opts|
-          extra = cfndsl_opts[:extras] || []
-          verbose = cfndsl_opts[:verbose] && STDERR
-          model = CfnDsl::eval_file_with_extras(opts[:filename], extra, verbose)
-          if opts[:output].nil?
-            verbose.puts("Writing to STDOUT") if verbose
-            STDOUT.puts model.to_json
-          else
-            verbose.puts("Writing to #{opts[:output]}") if verbose
-            output = File.open( File.expand_path(opts[:output]), "w")
-            output.puts model.to_json
-          end
+          generate(opts)
         end
       end
+    end
+
+    private
+
+    def generate(opts)
+      log(opts)
+      outputter(opts).puts model(opts[:filename])
+    end
+
+    def log(opts)
+      type = opts[:output].nil? ? "STDOUT" : opts[:output]
+      verbose.puts("Writing to #{type}") if verbose
+    end
+
+    def outputter(opts)
+      opts[:output].nil? ? STDOUT : file_output(opts[:output])
+    end
+
+    def model(filename)
+      fail "#{filename} doesn't exist" unless File.exist? filename
+      CfnDsl.eval_file_with_extras(filename, extra, verbose).to_json
+    end
+
+    def extra
+      cfndsl_opts.fetch(:extras, [])
+    end
+
+    def verbose
+      cfndsl_opts[:verbose] && STDERR
+    end
+
+    def file_output(path)
+      File.open(File.expand_path(path), "w")
     end
   end
 end

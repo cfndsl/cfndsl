aa6b7838bfa42c4ed41fda27c9be4af00426b29a
Steven Jack <stevenmajack@gmail.com>
2015-08-10 13:15:17 +0100

    Closes #122 and resolves #90 - Default FnNot to an array

--- lib/cfndsl/JSONable.rb
+++ lib/cfndsl/JSONable.rb
@@ -66,7 +66,7 @@ module CfnDsl
     def FnNot(value)
       ##
       # Equivalent to the Cloudformation template built in function Fn::Not
-      Fn.new("Not", value)
+      Fn.new("Not", [value].flatten.compact)
     end
 
     def FnOr(array)

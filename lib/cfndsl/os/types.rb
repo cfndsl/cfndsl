ad6d4099052c70e427a6bd96d7411d1a6ef5265b
Steven Jack <stevenmajack@gmail.com>
2016-03-18 17:09:50 +0000

    Move OS into own type and remove lib/types.rb

--- lib/cfndsl/os/types.rb
+++ lib/cfndsl/os/types.rb
@@ -4,68 +4,69 @@ require 'cfndsl/plurals'
 require 'cfndsl/names'
 
 module CfnDsl
-  module OSTypes
-    os_types = YAML.load(File.open("#{File.dirname(__FILE__)}/os_types.yaml"))
-    OSTypes.const_set('OS_Types', os_types)
+  module OS
+    module Types
+      os_types = YAML.load(File.open("#{File.dirname(__FILE__)}/types.yaml"))
+      Types.const_set('Types_Internal', os_types)
 
-    # Do a little sanity checking - all of the types referenced in Resources
-    # should be represented in Types
-    os_types['Resources'].keys.each do |resource_name|
-      resource = os_types['Resources'][resource_name]
-      resource.values.each do |thing|
-        thing.values.each do |type|
-          if type.is_a?(Array)
-            type.each do |inner_type|
-              puts "unknown type #{inner_type}" unless os_types['Types'].key?(inner_type)
+      # Do a little sanity checking - all of the types referenced in Resources
+      # should be represented in Types
+      os_types['Resources'].keys.each do |resource_name|
+        resource = os_types['Resources'][resource_name]
+        resource.values.each do |thing|
+          thing.values.each do |type|
+            if type.is_a?(Array)
+              type.each do |inner_type|
+                puts "unknown type #{inner_type}" unless os_types['Types'].key?(inner_type)
+              end
+            else
+              puts "unknown type #{type}" unless os_types['Types'].key?(type)
             end
-          else
-            puts "unknown type #{type}" unless os_types['Types'].key?(type)
           end
         end
       end
-    end
 
-    # All of the type values should also be references
+      # All of the type values should also be references
 
-    os_types['Types'].values do |type|
-      if type.respond_to?(:values)
-        type.values.each do |tv|
-          puts "unknown type #{tv}" unless os_types['Types'].key?(tv)
+      os_types['Types'].values do |type|
+        if type.respond_to?(:values)
+          type.values.each do |tv|
+            puts "unknown type #{tv}" unless os_types['Types'].key?(tv)
+          end
         end
       end
-    end
 
-    # declare classes for all of the types with named methods for setting the values
-    class OSType < JSONable
-    end
-
-    classes = {}
-
-    # Go through and declare all of the types first
-    os_types['Types'].each_key do |typename|
-      if !OSTypes.const_defined?(typename)
-        klass = OSTypes.const_set(typename, Class.new(OSType))
-        classes[typename] = klass
-      else
-        classes[typename] = OSTypes.const_get(typename)
+      # declare classes for all of the types with named methods for setting the values
+      class Type < JSONable
       end
-    end
 
-    # Now go through them again and define attribute setter methods
-    classes.each_pair do |typename, type|
-      typeval = os_types['Types'][typename]
-      next unless typeval.respond_to?(:each_pair)
-      typeval.each_pair do |attr_name, attr_type|
-        if attr_type.is_a?(Array)
-          klass = CfnDsl::OSTypes.const_get(attr_type[0])
-          variable = "@#{attr_name}".to_sym
+      classes = {}
 
-          method = CfnDsl::Plurals.singularize(attr_name)
-          methods = attr_name
-          all_methods = CfnDsl.method_names(method) + CfnDsl.method_names(methods)
-          type.class_eval do
-            all_methods.each do |method_name|
-              define_method(method_name) do |value = nil, *rest, &block|
+      # Go through and declare all of the types first
+      os_types['Types'].each_key do |typename|
+        if !Types.const_defined?(typename)
+          klass = Types.const_set(typename, Class.new(Type))
+          classes[typename] = klass
+        else
+          classes[typename] = Types.const_get(typename)
+        end
+      end
+
+      # Now go through them again and define attribute setter methods
+      classes.each_pair do |typename, type|
+        typeval = os_types['Types'][typename]
+        next unless typeval.respond_to?(:each_pair)
+        typeval.each_pair do |attr_name, attr_type|
+          if attr_type.is_a?(Array)
+            klass = CfnDsl::OS::Types.const_get(attr_type[0])
+            variable = "@#{attr_name}".to_sym
+
+            method = CfnDsl::Plurals.singularize(attr_name)
+            methods = attr_name
+            all_methods = CfnDsl.method_names(method) + CfnDsl.method_names(methods)
+            type.class_eval do
+              all_methods.each do |method_name|
+                define_method(method_name) do |value = nil, *rest, &block|
                 existing = instance_variable_get(variable)
                 # For no-op invocations, get out now
                 return existing if value.nil? && rest.empty? && !block
@@ -125,20 +126,21 @@ module CfnDsl
                   end
                 end
                 return existing
+                end
               end
             end
-          end
-        else
-          klass = CfnDsl::OSTypes.const_get(attr_type)
-          variable = "@#{attr_name}".to_sym
+          else
+            klass = CfnDsl::OS::Types.const_get(attr_type)
+            variable = "@#{attr_name}".to_sym
 
-          type.class_eval do
-            CfnDsl.method_names(attr_name) do |inner_method|
-              define_method(inner_method) do |value = nil, *_rest, &block|
+            type.class_eval do
+              CfnDsl.method_names(attr_name) do |inner_method|
+                define_method(inner_method) do |value = nil, *_rest, &block|
                 value ||= klass.new
                 instance_variable_set(variable, value)
                 value.instance_eval(&block) if block
                 value
+                end
               end
             end
           end

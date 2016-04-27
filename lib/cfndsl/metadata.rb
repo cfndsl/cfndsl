fc3383a95f21bbd6d5a871e572daf7fe422a228d
John Ferlito <johnf@inodes.org>
2016-04-27 16:11:09 +1000

    Use as_json instead of to_json (#157)

--- lib/cfndsl/metadata.rb
+++ lib/cfndsl/metadata.rb
@@ -3,14 +3,12 @@ require 'cfndsl/jsonable'
 module CfnDsl
   # Handles Metadata objects
   class MetadataDefinition < JSONable
+    include JSONSerialisableObject
+
     attr_reader :value
 
     def initialize(value)
       @value = value
-    end
-
-    def to_json(*a)
-      @value.to_json(*a)
     end
   end
 end
--- lib/cfndsl/properties.rb
+++ lib/cfndsl/properties.rb
@@ -9,13 +9,12 @@ module CfnDsl
   #   }
   #
   class PropertyDefinition < JSONable
+    include JSONSerialisableObject
+
     attr_reader :value
+
     def initialize(value)
       @value = value
-    end
-
-    def to_json(*a)
-      @value.to_json(*a)
     end
   end
 end

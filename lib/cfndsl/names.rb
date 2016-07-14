5279ec93a9b476221d262abad3e87d685bbc7ea6
Travis Dempsey <dempsey.travis@gmail.com>
2016-07-14 14:52:22 -0500

    refactor model_names method

--- lib/cfndsl/names.rb
+++ lib/cfndsl/names.rb
@@ -2,17 +2,9 @@
 module CfnDsl
   # iterates through the the valid case-insensitive names
   # for "name"
-  def self.method_names(name, &block)
-    if block
-      name_str = name.to_s
-      yield name_str.to_sym
-      n = name_str.dup
-      n[0] = n[0].swapcase
-      yield n.to_sym
-    else
-      result = [name.dup, name.dup]
-      result[1][0] = result[1][0].swapcase
-      return result
-    end
+  def self.method_names(name)
+    name_str = name.to_s.dup
+    names = [name_str, name_str.gsub(/^\w/, &:swapcase)]
+    block_given? ? names.each { |n| yield n.to_sym } : names
   end
 end

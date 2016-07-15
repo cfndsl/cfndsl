daf1dacdbf91b877e0d425fdcda17eba79bec5d3
Travis Dempsey <dempsey.travis@gmail.com>
2016-07-15 13:46:14 -0500

    use module_function

--- lib/cfndsl/names.rb
+++ lib/cfndsl/names.rb
@@ -1,8 +1,10 @@
 # Method name helper
 module CfnDsl
+  module_function
+
   # iterates through the the valid case-insensitive names
   # for "name"
-  def self.method_names(name)
+  def method_names(name)
     name_str = name.to_s.dup
     names = [name_str, name_str.gsub(/^\w/, &:swapcase)]
     block_given? ? names.each { |n| yield n.to_sym } : names

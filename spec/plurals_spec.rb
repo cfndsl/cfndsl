1569a8c6553d6cbb7bcb45ceeb21803a906f956c
Travis Dempsey <dempsey.travis@gmail.com>
2016-07-15 13:33:56 -0500

    spec for plurals module

---
+++ spec/plurals_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe CfnDsl::Plurals do
+  context '.pluralize' do
+    it 'pluralizes methods from the list' do
+      expect(described_class.pluralize(:SecurityGroupIngress)).to eq('SecurityGroupIngress')
+    end
+
+    it 'pluralizes other methods' do
+      expect(described_class.pluralize(:StageKey)).to eq('StageKeys')
+    end
+  end
+
+  context '.singularize' do
+    it 'singularizes methods from the list' do
+      expect(described_class.singularize(:SecurityGroupIngress)).to eq('SecurityGroupIngress')
+    end
+
+    it 'singularizes other methods' do
+      expect(described_class.singularize(:StageKeys)).to eq('StageKey')
+    end
+  end
+end

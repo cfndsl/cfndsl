dee7ce91ca0cf88e9c13ad9c787729c0b22bfa1e
Travis Dempsey <dempsey.travis@gmail.com>
2016-07-15 14:58:28 -0500

    added orchestration template specs for aws, os

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,5 @@ require 'aruba/rspec'
 
 bindir = File.expand_path('../../bin', __FILE__)
 ENV['PATH'] = [ENV['PATH'], bindir].join(':')
+
+Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |f| require f }

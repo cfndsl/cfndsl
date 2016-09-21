ee21678a70028f4b6655e30e06aeb7e9a5531765
Greg Cockburn <gergnz@gmail.com>
2016-09-21 14:05:26 +1000

    This supports yaml as an output type and leaves json as the default (#243)
    * Add support to print yaml instead of json, fixes #240
    
    * useless use of output
    
    * fix tests
    
    * switch to format type
    
    * fix tests

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -9,6 +9,7 @@ describe 'cfndsl', type: :aruba do
           -r, --ruby FILE                  Evaluate ruby file before template
           -j, --json FILE                  Import json file as local variables
           -p, --pretty                     Pretty-format output JSON
+          -f, --format FORMAT              Specify the output format (JSON default)
           -D, --define "VARIABLE=VALUE"    Directly set local VARIABLE as VALUE
           -v, --verbose                    Turn on verbose ouptut
           -b, --disable-binding            Disable binding configuration

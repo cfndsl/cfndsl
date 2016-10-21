393770d74053bbe4137c31916be237238e1594c1
Mike Chau <mike@chau.io>
2016-10-21 18:50:51 -0500

    Add TaskRoleArn property for ECS::TaskDefinition

---
+++ spec/aws/ecs_task_definition_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe CfnDsl::CloudFormationTemplate do
+  subject(:template) { described_class.new }
+
+  describe "#ECS_TaskDefinition" do
+    it 'supports TaskRoleArn property' do
+      template.ECS_TaskDefinition(:Test) do
+        TaskRoleArn 'arn:aws:iam::123456789012:role/S3Access'
+      end
+
+      expect(template.to_json).to include('"TaskRoleArn":"arn:aws:iam::123456789012:role/S3Access"')
+    end
+  end
+end

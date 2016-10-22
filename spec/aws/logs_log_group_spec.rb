d2460db003c30f61f0aa2296f3bfa2f7c4a41017
Mike Chau <mike@chau.io>
2016-10-21 19:06:56 -0500

    Fix lint violation for logs log group spec

--- spec/aws/logs_log_group_spec.rb
+++ spec/aws/logs_log_group_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe CfnDsl::CloudFormationTemplate do
   subject(:template) { described_class.new }
 
-  describe "#Logs_LogGroup" do
-    it 'supports RetentionInDays' do
+  describe '#Logs_LogGroup' do
+    it 'supports RetentionInDays property' do
       template.Logs_LogGroup(:Test) do
         RetentionInDays 7
       end
@@ -12,7 +12,7 @@ describe CfnDsl::CloudFormationTemplate do
       expect(template.to_json).to include('"RetentionInDays":7')
     end
 
-    it 'supports LogGroupName' do
+    it 'supports LogGroupName property' do
       template.Logs_LogGroup(:Test) do
         LogGroupName 'TestLogGroup'
       end

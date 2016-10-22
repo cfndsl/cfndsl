e5bf71ec4704268c452ca69d9c0182b67b5dccdb
Mike Chau <mike@chau.io>
2016-10-21 19:05:44 -0500

    Fix lint violation on ecs task definition spec

--- spec/aws/ecs_task_definition_spec.rb
+++ spec/aws/ecs_task_definition_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe CfnDsl::CloudFormationTemplate do
   subject(:template) { described_class.new }
 
-  describe "#ECS_TaskDefinition" do
+  describe '#ECS_TaskDefinition' do
     it 'supports TaskRoleArn property' do
       template.ECS_TaskDefinition(:Test) do
         TaskRoleArn 'arn:aws:iam::123456789012:role/S3Access'

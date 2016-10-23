ff97fce160f57e892caafc33481bb0a812329892
Mike Chau <mikechau@users.noreply.github.com>
2016-10-22 22:09:07 -0500

    Add support for LogGroupName Property (#262)
    * Add support for LogGroupName Property
    
    * Fix lint violation for logs log group spec

---
+++ spec/aws/logs_log_group_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe CfnDsl::CloudFormationTemplate do
+  subject(:template) { described_class.new }
+
+  describe '#Logs_LogGroup' do
+    it 'supports RetentionInDays property' do
+      template.Logs_LogGroup(:Test) do
+        RetentionInDays 7
+      end
+
+      expect(template.to_json).to include('"RetentionInDays":7')
+    end
+
+    it 'supports LogGroupName property' do
+      template.Logs_LogGroup(:Test) do
+        LogGroupName 'TestLogGroup'
+      end
+
+      expect(template.to_json).to include('"LogGroupName":"TestLogGroup')
+    end
+  end
+end

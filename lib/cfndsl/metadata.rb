2ba8cdb35dc09ca8a218b36c4a318f67fd44ff49
Michael Baker <michael.baker@bulletproof.net>
2016-11-16 12:46:54 +1100

    Updates

---
+++ lib/cfndsl/metadata.rb
@@ -0,0 +1,20 @@
+require 'cfndsl/jsonable'
+
+module CfnDsl
+  # Handles property objects for Resources
+  #
+  # Usage
+  #   Resource("aaa") {
+  #     Property("propName", "propValue" )
+  #   }
+  #
+  class MetadataDefinition < JSONable
+    include JSONSerialisableObject
+
+    attr_reader :value
+
+    def initialize(value)
+      @value = value
+    end
+  end
+end

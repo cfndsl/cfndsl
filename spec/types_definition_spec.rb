fd3e07b95b0a8f159c65bcbbb67a8af498263888
Travis Dempsey <dempsey.travis@gmail.com>
2017-05-10 23:20:17 -0500

    make sure all types defined, both new way and old, are correct

--- spec/types_definition_spec.rb
+++ spec/types_definition_spec.rb
@@ -1,20 +1,22 @@
 require 'spec_helper'
 
+# This is a somewhat temporary test class to compare functionality
+# between the AWS, OS and new ways of defining types
 RSpec.describe 'Type Definitions' do
-  aws_yaml = YAML.load_file File.expand_path('../../lib/cfndsl/aws/types.yaml', __FILE__)
-  os_yaml = YAML.load_file File.expand_path('../../lib/cfndsl/os/types.yaml', __FILE__)
-  new_yaml = YAML.load_file File.expand_path('../../new_types.yaml', __FILE__)
+  aws_spec = YAML.load_file File.expand_path('../../lib/cfndsl/aws/types.yaml', __FILE__)
+  os_spec = YAML.load_file File.expand_path('../../lib/cfndsl/os/types.yaml', __FILE__)
+  new_spec = CfnDsl::Specification.extract_from_resource_spec!
 
-  { 'AWS' => aws_yaml, 'OS' => os_yaml, 'New' => new_yaml }.each_pair do |cloud, yaml|
+  { 'AWS' => aws_spec, 'OS' => os_spec, 'New' => new_spec }.each_pair do |cloud, specdef|
     context cloud do
-      resources = yaml['Resources']
-      types = yaml['Types']
+      resources = specdef['Resources']
+      types = specdef['Types']
 
       context 'Resources' do
         resources.each do |name, info|
           it "#{name} has all property types defined" do
             properties = info['Properties']
-            properties.each do |name, type|
+            properties.each do |_, type|
               type = type.first if type.is_a?(Array)
               expect(types).to have_key(type)
             end
@@ -29,9 +31,9 @@ RSpec.describe 'Type Definitions' do
             if type.is_a?(String)
               expect(types).to have_key(type)
             elsif type.is_a?(Hash)
-              type.values.flatten.each{ |t| expect(types).to have_key(t) }
+              type.values.flatten.each { |t| expect(types).to have_key(t) }
             else
-              fail "A defined type should only be of the form String, Array or Hash, got #{type.class}"
+              raise "A defined type should only be of the form String, Array or Hash, got #{type.class}"
             end
           end
         end

b36d3ea5a7444ae489946e6099e3ca5a3aacfd87
Greg Cockburn <gergnz@gmail.com>
2019-01-29 07:19:15 +1100

    add a test for the plural

--- spec/aws/nested_arrays_spec.rb
+++ spec/aws/nested_arrays_spec.rb
@@ -49,5 +49,29 @@ describe CfnDsl::CloudFormationTemplate do
       expect(template.to_json).not_to include('"ArtifactStores":{"Location":"mybucket","Type":"S3"}')
       expect(template.to_json).not_to include('"ArtifactStores":[{"Location":"mybucket","Type":"S3"}]')
     end
+
+    it 'check ArtifactStores is an array' do
+      template.CodePipeline_Pipeline(:Test) do
+        ArtifactStores [
+          {
+            ArtifactStore: {
+              Type: 'S3',
+              Location: 'mybucket',
+              EncryptionKey: {
+                Id: 'arn:goes:here',
+                Type: 'KMS'
+              }
+            },
+            Region: Ref('AWS::Region')
+          }
+        ]
+      end
+
+      # rubocop:disable Metrics/LineLength
+      expect(template.to_json).to include('"ArtifactStores":[{"ArtifactStore":{"Type":"S3","Location":"mybucket","EncryptionKey":{"Id":"arn:goes:here","Type":"KMS"}},"Region":{"Ref":"AWS::Region"}}]')
+      # rubocop:enable Metrics/LineLength
+      expect(template.to_json).not_to include('"ArtifactStore":{"Location":"mybucket","Type":"S3"}')
+      expect(template.to_json).not_to include('"ArtifactStores":{"Location":"mybucket","Type":"S3"}')
+    end
   end
 end

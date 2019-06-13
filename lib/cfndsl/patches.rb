826db7cbc99ff03c99c1cf9d8c9eff481de74f03
Steven Tan <steven.tan@bulletproof.net>
2019-06-13 15:37:37 +1000

    Added cfndsl patch for AWSEC2CapacityReservationTag Name Error

--- lib/cfndsl/patches.rb
+++ lib/cfndsl/patches.rb
@@ -117,6 +117,12 @@ module CfnDsl
             'Key' => { 'PrimitiveType' => 'String' }
           }
         },
+        'AWS::EC2::CapacityReservation.Tag' => {
+          'Properties' => {
+            'Value' => { 'PrimitiveType' => 'String' },
+            'Key' => { 'PrimitiveType' => 'String' }
+          }
+        },
         'AWS::DLM::LifecyclePolicy.Tag' => {
           'Properties' => {
             'Value' => { 'PrimitiveType' => 'String' },

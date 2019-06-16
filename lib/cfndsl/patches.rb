9855796153db615906fe2f5a9f571e9d59cc79ef
Greg Cockburn <gergnz@gmail.com>
2019-06-16 11:09:14 +1000

    add the latest patches

--- lib/cfndsl/patches.rb
+++ lib/cfndsl/patches.rb
@@ -135,6 +135,12 @@ module CfnDsl
             'Key' => { 'PrimitiveType' => 'String' }
           }
         },
+        'AWS::EC2::ClientVpnEndpoint.Tag' => {
+          'Properties' => {
+            'Value' => { 'PrimitiveType' => 'String' },
+            'Key' => { 'PrimitiveType' => 'String' }
+          }
+        },
         'AWS::Serverless::SimpleTable.PrimaryKey' => {
           'Properties' => {
             'Name' => { 'PrimitiveType' => 'String' },

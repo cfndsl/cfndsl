c9cac7104b7028800b438ae0fa1fe370344e9f58
Cam Maxwell <camm@amazon.com>
2019-09-04 23:34:31 +1000

    #414 Added support for Rule-specific intrinsic functions as per https://docs.aws.amazon.com/servicecatalog/latest/adminguide/intrinsic-function-reference-rules.html#supported-rule-functions

--- lib/cfndsl/rules.rb
+++ lib/cfndsl/rules.rb
@@ -5,11 +5,48 @@ require 'cfndsl/jsonable'
 module CfnDsl
   # Handles Rule objects
   class RuleDefinition < JSONable
-    dsl_attr_setter :RuleCondition, :Assertions
+    dsl_attr_setter :RuleCondition, :Assertion
 
     def initialize(value = nil)
-      @Assertions = value[:Assertions] if value[:Assertions]
-      @RuleCondition = value[:RuleCondition] if value[:RuleCondition]
+      puts value
+      @Assertions = []
+      # @Assertions = []value[:Assertions] if value[:Assertions]
+      # @RuleCondition = value[:RuleCondition] if value[:RuleCondition]
     end
+
+    def RuleCondition(value)
+      @RuleCondition = value
+    end 
+
+    def Assert(desc, struct)
+      @Assertions.push({ "Assert" => struct, "AssertDescription" => desc })
+    end
+
+    def FnContains(list_of_strings, string)
+      Fn.new('Contains', [list_of_strings, string])
+    end
+
+    def FnEachMemberEquals(list_of_strings, string)
+      Fn.new('EachMemberEquals', [list_of_strings, string])
+    end
+
+    def FnEachMemberIn(strings_to_check, strings_to_match)
+      Fn.new('EachMemberIn', [strings_to_check, strings_to_match])
+    end
+
+    def FnRefAll(parameter_type)
+      Fn.new('RefAll', parameter_type)
+    end
+
+    def FnValueOf(parameter_logical_id, attribute)
+      raise "Cannot use functions within FnValueOf" unless parameter_logical_id.is_a?(String) && attribute.is_a?(String)
+      Fn.new('ValueOf', [parameter_logical_id, attribute])
+    end
+
+    def FnValueOfAll(parameter_logical_id, attribute)
+      raise "Cannot use functions within FnValueOfAll" unless parameter_logical_id.is_a?(String) && attribute.is_a?(String)
+      Fn.new('ValueOfAll', [parameter_logical_id, attribute])
+    end
+
   end
 end
